6eea3e96c37ff55d75342aeb4a7d06fda723da0b
elianrichard <elianrichards@gmail.com>
2023-04-13 19:47:33 +0700

    added headshots and fixed crew sequence

--- src/static/crew-list.ts
+++ src/static/crew-list.ts
@@ -1,49 +1,81 @@
-import CrewImg from "@/assets/images/crew-1.jpg";
+import CrewImg from "@/assets/images/crews/placeholder.jpg";
+import ChaImg from "@/assets/images/crews/Cha.webp";
+import CharlesImg from "@/assets/images/crews/Charles.webp";
+import CherImg from "@/assets/images/crews/Cher.webp";
+import ChloeImg from "@/assets/images/crews/Chloe.webp";
+import ChrisImg from "@/assets/images/crews/Chris.webp";
+import DeniseImg from "@/assets/images/crews/Denise.webp";
+import DesmondImg from "@/assets/images/crews/Desmond.webp";
+import EricImg from "@/assets/images/crews/Eric.webp";
+import FernandaImg from "@/assets/images/crews/Fernanda.webp";
+import GisselleImg from "@/assets/images/crews/Gisselle.webp";
+import IndiaImg from "@/assets/images/crews/India.webp";
+import JacksonImg from "@/assets/images/crews/Jackson.webp";
+import JasonImg from "@/assets/images/crews/Jason.webp";
+import JoseImg from "@/assets/images/crews/Jose.webp";
+import JoshCTImg from "@/assets/images/crews/Josh_CT.webp";
+import JoshRImg from "@/assets/images/crews/Josh_R.webp";
+import JoyImg from "@/assets/images/crews/Joy.webp";
+import KuanImg from "@/assets/images/crews/Kuan.webp";
+import MartinImg from "@/assets/images/crews/Martin.webp";
+import MeilinImg from "@/assets/images/crews/Meilin.webp";
+import MickeyImg from "@/assets/images/crews/Mickey.webp";
+import MJImg from "@/assets/images/crews/MJ.webp";
+import NarejImg from "@/assets/images/crews/Narej.webp";
+import NicholasImg from "@/assets/images/crews/Nicholas.webp";
+import NinaImg from "@/assets/images/crews/Nina.webp";
+import OscarImg from "@/assets/images/crews/Oscar.webp";
+import PyaeCindyImg from "@/assets/images/crews/Pyae_Cindy.webp";
+import RamImg from "@/assets/images/crews/Ram.webp";
+import RicardoImg from "@/assets/images/crews/Ricardo.webp";
+import ScottImg from "@/assets/images/crews/Scott.webp";
+import TadImg from "@/assets/images/crews/Tad.webp";
+import TylerImg from "@/assets/images/crews/Tyler.webp";
 
 export const headList = [
   {
     name: "Haoqian (Eric) He",
     role: "Director",
-    image: CrewImg,
+    image: EricImg,
     bio: "Haoqian (Eric) He is a CSUN international student majoring in film production. He comes from Xiamen, China, and spent almost four years studying film production in Los Angeles, California.",
   },
   {
     name: "Ricardo Medina",
     role: "Co-Director",
-    image: CrewImg,
+    image: RicardoImg,
     bio: "JR Ricardo Medina is a first-generation College student who moved to Northridge from Chino, California, to pursue his dreams in filmmaking. Ricardo Medina is a CSUN student majoring in Film Production with an emphasis in Cinematography and will graduate in the Fall of 2023.",
   },
 ];
 
 export const crewList = [
   {
+    name: "Gisselle Olmedo",
+    role: "Associate Producer",
+    image: GisselleImg,
+    bio: "Gisselle Olmedo Torres is a first generation Cal State Northridge senior, majoring in Film Production with a concentration in Producing. Born and raised in the San Fernando Valley, with roots from both Mexico and El Salvador, she strives to create pathways for the stories of underrepresented communities.",
+  },
+  {
     name: "Charles Lai ",
     role: "Producer",
-    image: CrewImg,
+    image: CharlesImg,
     bio: "Charles Lai is a CSUN film production major with an emphasis on producing and a minor in business management. He has experience in directing, writing, producing, being an AD, and digital illustrating.",
   },
   {
     name: "Fernanda Zaragoza",
     role: "Producer",
-    image: CrewImg,
+    image: FernandaImg,
     bio: "Fernanda Zaragoza is a first generation college student at CSUN majoring in production with an emphasis in producing.  She was born and raised in Los Angeles, California and began her filmmaking career as a freshman in high school.",
   },
   {
     name: "Desmond Loh",
     role: "Producer",
-    image: CrewImg,
+    image: DesmondImg,
     bio: "Desmond Qi Hong Loh is a Malaysian international student currently in his junior year majoring in Film Production with an emphasis on Producing.",
-  },
-  {
-    name: "Gisselle Olmedo",
-    role: "Associate Producer",
-    image: CrewImg,
-    bio: "Gisselle Olmedo Torres is a first generation Cal State Northridge senior, majoring in Film Production with a concentration in Producing. Born and raised in the San Fernando Valley, with roots from both Mexico and El Salvador, she strives to create pathways for the stories of underrepresented communities.",
   },
   {
     name: "Scott Tan",
     role: "1st A.D",
-    image: CrewImg,
+    image: ScottImg,
     bio: "Scott is an international student from Malaysia. A film production student in his senior year with an emphasis on producing and a minor in History, Scott has worked on several student films and helped produced the senior thesis film, False Profits.",
   },
   {
@@ -55,163 +87,169 @@ export const crewList = [
   {
     name: "India Sides",
     role: "2nd, 2nd A.D",
-    image: CrewImg,
+    image: IndiaImg,
     bio: "India sides is a third year at Cal state Northridge, majoring in Entertainment Media Management. Born and raised in the San Fernando valley, she began acting at a young age. This opened a door for her and began her experience working on sets.",
   },
   {
     name: "Martin Avina",
     role: "Script Supervisor",
-    image: CrewImg,
+    image: MartinImg,
     bio: "Martin is a CSUN Film Production major with an emphasis on Producing and a minor in Business Law. Having extensive experience in producing, script supervising, writing, and directing, Martin still has the desire to learn more.",
   },
   {
     name: "Joyfiana Venessa",
     role: "Director of Photography",
-    image: CrewImg,
+    image: JoyImg,
     bio: "Joyfiana Venessa Suryakusuma is a cinematographer, writer, director from Jakarta, Indonesia. She moved to Los Angeles to pursue a career in filmmaking and is currently a senior in CSUN, majoring in film production with an emphasis in cinematography and a minor in Photography.",
   },
   {
     name: "Cher Martinez",
     role: "Camera Operator",
-    image: CrewImg,
+    image: CherImg,
     bio: "Cher Martinez is a Mexican filmmaker and photographer. They are a first generation film production student at CSUN with an emphasis in Cinematography, and transfer from Santa Barbara City College where she grew up.",
   },
   {
     name: "Kuan Wei Yu ",
     role: "1st A.C",
-    image: CrewImg,
+    image: KuanImg,
     bio: "Kuan Wei Yu is a junior majoring in film production with an emphasis in cinematography. He was born and raised in Taiwan and moved to the US in 2017 to pursue an education and career in film. What drew him to filmmaking is the magical way that movies make the audience feel.",
   },
   {
     name: "Jason Paullus",
     role: "2nd A.C",
-    image: CrewImg,
+    image: JasonImg,
     bio: "Jason Paullus is a senior at CSUN majoring in film production with an emphasis in cinematography. He is originally from central Indiana, and moved out to Northridge, California for school in August 2021.",
-  },
-  {
-    name: "Oscar Jimenez",
-    role: "Editor & DIT",
-    image: CrewImg,
-    bio: "Oscar Jimenez is a third-year student at CSUN. Born and raised in the San Fernando Valley, he is the third child of two Mexican immigrants. He is currently majoring in Film Production with an emphasis on Editing and minoring in photography.",
-  },
-  {
-    name: "Ram Margalit",
-    role: "Editor",
-    image: CrewImg,
-    bio: "Ram Margalit is a fourth year film production major with an emphasis in editing.  He's always enjoyed visual effects, motion graphics, and of course, editing. He's done editing work for hire but has also had moderate success with personal projects, reaching a niche audience with a similar esoteric sense of humor.",
-  },
-  {
-    name: "Josh Rattí",
-    role: "Key Grip & Post Sound",
-    image: CrewImg,
-    bio: "Joshua Rattí is a highly skilled film production major with a focus on cinematography, sound, and editing. His passion for photography and film production began during his high school education, where he started making short films with his friends.",
-  },
-  {
-    name: "Jackson Geary",
-    role: "Best Boy Grip",
-    image: CrewImg,
-    bio: "Jackson Geary (he/him) is a filmmaker born and raised in San Diego, California. Forming his passion for storytelling at a young age creating various skits with his relatives, he later went on to participate and create films in the Coronado School of the Arts Program where he blossomed his creativity and found a deep passion for the camera.",
-  },
-  {
-    name: "Charissa Legoh",
-    role: "Art PA",
-    image: CrewImg,
-    bio: "Charissa Legoh is a fourth-year, international student from Indonesia pursuing a degree in Entertainment Media Management at CSUN. She aspires to have a career in post production as a film colorist.",
   },
   {
     name: "Nareh Dovlatyan",
     role: "Gaffer",
-    image: CrewImg,
+    image: NarejImg,
     bio: "Nareh Dovlatyan is an Armenian-American filmmaker who aspires to be a skilled storyteller through cinematography. She is a gaffer and set electrician, who is currently studying the process of film production at CSUN.",
   },
   {
     name: "Tyler Jones-Juarez",
     role: "Best Boy Electric",
-    image: CrewImg,
+    image: TylerImg,
     bio: "Tyler Jones-Juárez is a senior at CSUN, majoring in Film Production with an emphasis in cinematography. He has a passion for bringing his ideas to life in the form of filmmaking. Ever since he was young, he has enjoyed taking photos, and capturing memories with his friends and family.",
+  },
+  {
+    name: "Josh Rattí",
+    role: "Key Grip & Post Sound",
+    image: JoshRImg,
+    bio: "Joshua Rattí is a highly skilled film production major with a focus on cinematography, sound, and editing. His passion for photography and film production began during his high school education, where he started making short films with his friends.",
+  },
+  {
+    name: "Hye Young",
+    role: "Best Girl Grip",
+    image: CrewImg,
+    bio: "Hye Young is a Senior at CSUN, graduating in Fall of 2023. Hye Young is majoring in Film production and is eager to utilize their skills for Your Promise production.",
+  },
+  {
+    name: "Jackson Geary",
+    role: "Best Boy Grip",
+    image: JacksonImg,
+    bio: "Jackson Geary (he/him) is a filmmaker born and raised in San Diego, California. Forming his passion for storytelling at a young age creating various skits with his relatives, he later went on to participate and create films in the Coronado School of the Arts Program where he blossomed his creativity and found a deep passion for the camera.",
+  },
+  {
+    name: "Oscar Jimenez",
+    role: "Editor & DIT",
+    image: OscarImg,
+    bio: "Oscar Jimenez is a third-year student at CSUN. Born and raised in the San Fernando Valley, he is the third child of two Mexican immigrants. He is currently majoring in Film Production with an emphasis on Editing and minoring in photography.",
+  },
+  {
+    name: "Ram Margalit",
+    role: "Editor & VFX Artist",
+    image: RamImg,
+    bio: "Ram Margalit is a fourth year film production major with an emphasis in editing.  He's always enjoyed visual effects, motion graphics, and of course, editing. He's done editing work for hire but has also had moderate success with personal projects, reaching a niche audience with a similar esoteric sense of humor.",
   },
   {
     name: "Tzu-Ni 'Nina' Guo",
     role: "Production Sound Mixer",
-    image: CrewImg,
+    image: NinaImg,
     bio: "Tzu-Ni Guo is a senior in Film Production major with an emphasis in sound. She is an international student from Taiwan, also a transfer student from Santa Monica College. She discovered her interest in filmmaking when she made and won an award for her first short film in high school.",
   },
   {
     name: "Josh Corbin-Tomlinson",
-    role: "Sound Boom",
-    image: CrewImg,
+    role: "Sound Boom Operator",
+    image: JoshCTImg,
     bio: "Josh Corbin-Tomlinson is a Senior in CSUN's Film production with an emphasis in sound. Josh began pursuing his love for making films at age ten to sharpen his technical skills and understanding of the medium. In 2021, he graduated with an AS-T in Film/TV and Electronic Media from the College of the Desert.",
-  },
-  {
-    name: "Tad Miller",
-    role: "Sound Utility",
-    image: CrewImg,
-    bio: "Tadato Miller is an senior student Film Production Sound Major with an emphasis on sound. Has experience as a Sound Mixer for 'Frederick Douglass Proof of Concept' a short film directed by Elvis Nolasco who won Critics Choice Award and a Golden Globe.",
   },
   {
     name: "Chloë Benz",
     role: "Sound Utility",
-    image: CrewImg,
+    image: ChloeImg,
     bio: "Chloë Benz is a senior at California State University Northridge pursuing a bachelor’s degree in Film Production with an emphasis in sound design. Before transferring to CSUN, Chloë attended De Anza College in the Bay Area and received two Associate degrees in Film Production and Animation.",
   },
   {
-    name: "Mickey Hao",
-    role: "Post Sound",
-    image: CrewImg,
-    bio: "Mickey Hao is a senior at Cal State Northridge, graduating in Fall of 2023. He is majoring in Film production with an emphasis in sound. Mickey has participated in numerous other CSUN senior thesis projects, as well as working in many other productions in the sound department.",
+    name: "Tad Miller",
+    role: "Sound Utility",
+    image: TadImg,
+    bio: "Tadato Miller is an senior student Film Production Sound Major with an emphasis on sound. Has experience as a Sound Mixer for 'Frederick Douglass Proof of Concept' a short film directed by Elvis Nolasco who won Critics Choice Award and a Golden Globe.",
   },
   {
     name: "Chris Woodland",
     role: "Post Sound Mixer",
-    image: CrewImg,
+    image: ChrisImg,
     bio: "Christopher Woodland is 21 years old and a film major with an emphasis in editing at California State University Northridge. Chris grew up in the Santa Clarita Valley and got his interest for film from his highschools tv program WRTV.",
+  },
+  {
+    name: "Mickey Hao",
+    role: "Post Sound",
+    image: MickeyImg,
+    bio: "Mickey Hao is a senior at Cal State Northridge, graduating in Fall of 2023. He is majoring in Film production with an emphasis in sound. Mickey has participated in numerous other CSUN senior thesis projects, as well as working in many other productions in the sound department.",
   },
   {
     name: "Pyae Phyo Pying (Cindy)",
     role: "Production Designer",
-    image: CrewImg,
+    image: PyaeCindyImg,
     bio: "Pyae Phyo Pying (Cindy) is a CSUN Senior in the Film Production program with an emphasis in Sound. She was born and raised in Mandalay, Myanmar. The experiences of growing up in both Burmese and Chinese cultures have brought her a sense of creativity.",
   },
   {
     name: "MJ Garza",
     role: "Art Director",
-    image: CrewImg,
+    image: MJImg,
     bio: "MJ Garza is a sound option student entering her third semester in the film production major. While she is focusing on sound, MJ also enjoys many aspects of filmmaking including production design, writing, producing, and directing.",
+  },
+  {
+    name: "Charissa Legoh",
+    role: "Art PA",
+    image: ChaImg,
+    bio: "Charissa Legoh is a fourth-year, international student from Indonesia pursuing a degree in Entertainment Media Management at CSUN. She aspires to have a career in post production as a film colorist.",
   },
   {
     name: "Meillin Liu",
     role: "Set Dresser",
-    image: CrewImg,
+    image: MeilinImg,
     bio: "Meillin Liu comes from China and moved to Los Angeles to pursue her dream as a filmmaker. She is currently a junior at CSUN, majoring in film production with an emphasis on producing and minoring in theatre with an emphasis on acting/directing.",
-  },
-  {
-    name: "Denise Maldonado",
-    role: "BTS Head and Editor",
-    image: CrewImg,
-    bio: "Denise Maldonado is a first-generation Mexican-American student at CSUN. She was drawn into the world of filmmaking in her early teenage years through a short-form video-hosting service named Vine.",
-  },
-  {
-    name: "Jose Ubaya",
-    role: "BTS Photographer",
-    image: CrewImg,
-    bio: "Jose Ubaya is a Mexican filmmaker and photographer. He is a senior at CSUN and is finishing up his degree in screenwriting, becoming a first-generation college student. He has lived in the United States for 19 years in the small town of Placentia, CA before moving to Northridge.",
   },
   {
     name: "Nicholas Tjen",
     role: "BTS Videographer",
-    image: CrewImg,
+    image: NicholasImg,
     bio: "He moved to the United States in 2018 to pursue his dream to be an impactful filmmaker. Currently, he is in his second semester in the Film Production option with an emphasis on editing.",
   },
   {
-    name: "Steven Han",
-    role: "Music Composer",
-    image: CrewImg,
-    bio: "Yuchen 'Steven' Han are senior music major students from China. He has his music studio and often work on some post-sound and soundtracks production for films and TV shows. Steven also studied at the Sony Records headquarters in Japan before the pandemic.",
+    name: "Jose Ubaya",
+    role: "BTS Photographer",
+    image: JoseImg,
+    bio: "Jose Ubaya is a Mexican filmmaker and photographer. He is a senior at CSUN and is finishing up his degree in screenwriting, becoming a first-generation college student. He has lived in the United States for 19 years in the small town of Placentia, CA before moving to Northridge.",
+  },
+  {
+    name: "Denise Maldonado",
+    role: "BTS Head and Editor",
+    image: DeniseImg,
+    bio: "Denise Maldonado is a first-generation Mexican-American student at CSUN. She was drawn into the world of filmmaking in her early teenage years through a short-form video-hosting service named Vine.",
   },
   {
     name: "Shuyu Hen",
     role: "Music Composer",
     image: CrewImg,
     bio: "Shuyu Shen are senior music major students from China. He has his music studio and often work on some post-sound and soundtracks production for films and TV shows. Shuyu have worked with Eric in the past couple of years on several film projects..",
+  },
+  {
+    name: "Steven Han",
+    role: "Music Composer",
+    image: CrewImg,
+    bio: "Yuchen 'Steven' Han are senior music major students from China. He has his music studio and often work on some post-sound and soundtracks production for films and TV shows. Steven also studied at the Sony Records headquarters in Japan before the pandemic.",
   },
 ];
