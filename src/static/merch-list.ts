fce02cf166682eb9514ff44b58a59e3cd8eed375
elianrichard <elianrichards@gmail.com>
2023-05-24 19:52:55 +0700

    changed merch image

--- src/static/merch-list.ts
+++ src/static/merch-list.ts
@@ -1,17 +1,21 @@
-import Merch1Front from "@/assets/images/merch/Merch1Front.webp";
-import Merch1Back from "@/assets/images/merch/Merch1Back.webp";
-import Merch2Front from "@/assets/images/merch/Merch2Front.webp";
-import Merch2Back from "@/assets/images/merch/Merch2Back.webp";
-import Merch3Front from "@/assets/images/merch/Merch3Front.webp";
-import Merch3Back from "@/assets/images/merch/Merch3Back.webp";
-import Merch4Front from "@/assets/images/merch/Merch4Front.webp";
-import Merch4Back from "@/assets/images/merch/Merch4Back.webp";
+import Merch1a from "@/assets/images/merch/Merch1a.webp";
+import Merch2a from "@/assets/images/merch/Merch2a.webp";
+import Merch3a from "@/assets/images/merch/Merch3a.webp";
+import Merch4a from "@/assets/images/merch/Merch4a.webp";
+import Merch1b from "@/assets/images/merch/Merch1b.webp";
+import Merch2b from "@/assets/images/merch/Merch2b.webp";
+import Merch3b from "@/assets/images/merch/Merch3b.webp";
+import Merch4b from "@/assets/images/merch/Merch4b.webp";
+import Merch1c from "@/assets/images/merch/Merch1c.webp";
+import Merch2c from "@/assets/images/merch/Merch2c.webp";
+import Merch3c from "@/assets/images/merch/Merch3c.webp";
+import Merch4c from "@/assets/images/merch/Merch4c.webp";
 
 const merchLists = [
-  [Merch1Front, Merch1Back],
-  [Merch2Front, Merch2Back],
-  [Merch3Front, Merch3Back],
-  [Merch4Front, Merch4Back],
+  [Merch1a, Merch1b, Merch1c],
+  [Merch2a, Merch2b, Merch2c],
+  [Merch3a, Merch3b, Merch3c],
+  [Merch4a, Merch4b, Merch4c],
 ];
 
 export default merchLists;
